233c3a3aca0155aebe40a1bb9118098178c0df0a
cjlora <cjlora@gmail.com>
2021-10-11 14:23:14 +0800

    integrated play games sdk

---
+++ Assets/PlayGames.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using TMPro;
+using System;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+
+public class PlayGames : MonoBehaviour
+{
+    public TextMeshProUGUI playerScore;
+    string leaderboardID = "CgkI6_ys_q8CEAIQAQ";
+    string achievementID = "CgkI6_ys_q8CEAIQAA";
+    public static PlayGamesPlatform platform;
+
+    void Start()
+    {
+        if (platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            platform = PlayGamesPlatform.Activate();
+        }
+
+        Social.Active.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                Debug.Log("Logged in successfully");
+            }
+            else
+            {
+                Debug.Log("Login Failed");
+            }
+        });
+        UnlockAchievement();
+    }
+
+    public void AddScoreToLeaderboard()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            Social.ReportScore(int.Parse(playerScore.text), leaderboardID, success => { });
+        }
+    }
+
+    public void ShowLeaderboard()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            platform.ShowLeaderboardUI();
+        }
+    }
+
+    public void ShowAchievements()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            platform.ShowAchievementsUI();
+        }
+    }
+
+    public void UnlockAchievement()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            Social.ReportProgress(achievementID, 100f, success => { });
+        }
+    }
+}
